6b8b9d3875bdd5dbe0b60953865c010975e0742e
Olivier Vastel <vastel.olivier@gmail.com>
2023-11-07 14:31:28 +0100

    Exo 3 finit select officier du pays 5

--- ex3.sql
+++ ex3.sql
@@ -1,2 +1,3 @@
-/* Requête 2. Sélectionner tous les noms des entités */
-SELECT name FROM entity
+/*Requête 3. Sélectionner tous les officiers d'un pays spécifique (le pays avec l'ID 5)*/
+SELECT * FROM officer
+WHERE country_id = 5
